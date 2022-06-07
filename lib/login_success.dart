85c08bf7d6ac9004406e46ab43e2982fbf3a587c
IzhanArif <izhanrf@gmail.com>
2022-06-08 00:19:40 +0500

    Task Done

---
+++ lib/login_success.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoginSuccess extends StatelessWidget {
+  const LoginSuccess({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("WELCOME! LOGIN SUCCESS")),
+      ),
+      body: Center(
+        child: ElevatedButton.icon(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.arrow_back,
+              size: 25.0,
+            ),
+            label: Text("Back")),
+      ),
+    );
+  }
+}
